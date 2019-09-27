02cc94178b3333e7b2046af9a322272f5751267d
dev <dev@sharingtest.local>
2019-09-27 13:19:57 +0800

    X

--- src/app/adobe-guard.service.ts
+++ src/app/adobe-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from "@angular/router";
+import { CanActivate } from '@angular/router';
 import { AdobeAnalyticService } from './adobe-analytic.service';
 
 @Injectable({
@@ -9,10 +9,8 @@ export class AdobeGuardService implements CanActivate {
 
   constructor(private adobeSvc: AdobeAnalyticService) { }
   canActivate(data) {
-    let path = data.url[0].path;
+    const path = data.url[0].path;
     this.adobeSvc.trackPage(path);
-
- 
     return true;
   }
 }
