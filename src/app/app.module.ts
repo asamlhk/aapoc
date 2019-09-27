51b8f2d797bd4d1f5ccd0c28aba4e059fe0142e8
dev <dev@sharingtest.local>
2019-09-27 15:28:42 +0800

    X

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { Page2Component } from './page2/page2.component';
 import { AdobeGuardService } from './adobe-guard.service'
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { BinputDirective } from './binput.directive';
-
+import { Page3Component } from './page3/page3.component';
+import { WebcamModule } from 'ngx-webcam';
+import { FormsModule } from '@angular/forms';
 const appRoutes: Routes = [
   { path: 'page1', component: Page1Component, canActivate: [AdobeGuardService], },
   { path: 'page2', component: Page2Component, canActivate: [AdobeGuardService], },
-
+  { path: 'page3', component: Page3Component, canActivate: [AdobeGuardService], },
 ];
 
 @NgModule({
@@ -20,12 +22,15 @@ const appRoutes: Routes = [
     AppComponent,
     Page1Component,
     Page2Component,
-    BinputDirective
+    BinputDirective,
+    Page3Component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     PdfViewerModule,
+    WebcamModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- debugging purposes only
